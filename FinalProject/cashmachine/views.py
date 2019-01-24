69d171d3225a1ad0fbf9d2b69677fa9553a8a207
konradchmielewski <konradchmielewski.moe@gmail.com>
2019-01-24 14:36:36 +0100

    Version 2

--- FinalProject/cashmachine/views.py
+++ FinalProject/cashmachine/views.py
@@ -15,18 +15,32 @@ class MainView(View):
 
         wanted_amount = request.POST.get("valueof")
 
-        if wanted_amount[0] is not "[" and wanted_amount[0] is not "Y":
+        if not wanted_amount:
+            return render(request, "cashmachine.html", {'notes': "[Empty Set]", 'disabled': 'disabled'})
+
+        try:
             wanted_amount = int(wanted_amount)
 
-            if wanted_amount % 10 == 0 and wanted_amount > 0:
-                for available_note in available_notes:
-                    note_amount = int(wanted_amount / available_note)
-                    for i in range(0, note_amount):
-                        notes.append(available_note)
-                        wanted_amount -= available_note
+            if wanted_amount > 0:
+                if wanted_amount % 10 == 0:
+                    if wanted_amount > 0:
+                        for available_note in available_notes:
+                            note_amount = int(wanted_amount / available_note)
+                            for i in range(0, note_amount):
+                                notes.append(available_note)
+                                wanted_amount -= available_note
+                        return render(request, "cashmachine.html", {'notes': str(notes), 'disabled': 'disabled'})
+                    else:
+                        return render(request, "cashmachine.html",
+                                      {'notes': "InvalidArgumentException", 'disabled': 'disabled'})
+                else:
+                    return render(request, "cashmachine.html", {'notes': "NoteUnavailableException",
+                                                                'disabled': 'disabled'})
             else:
-                return render(request, "cashmachine.html", {'notes': str(notes), 'disabled': 'disabled'})
-            return render(request, "cashmachine.html", {'notes': str(notes),  'disabled': 'disabled'})
-        else:
-            return render(request, "cashmachine.html", {'notes': "You already received your notes!", 'disabled': 'disabled'})
-
+                return render(request, "cashmachine.html",
+                              {'notes': "InvalidArgumentException", 'disabled': 'disabled'})
+        except IndexError:
+            return render(request, "cashmachine.html", {'notes': "[Empty Set]", 'disabled': 'disabled'})
+        except ValueError:
+            return render(request, "cashmachine.html",
+                          {'notes': "InvalidArgumentException", 'disabled': 'disabled'})
